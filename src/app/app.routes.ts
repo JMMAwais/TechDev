0c77347e7d80a095d6e8ee5eff2a15c269e4e1d7
JMMAwais <jmm.awaiskiyani@gmail.com>
2025-09-27 23:17:29 +0500

    Footer added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { PricingComponent } from './components/pricing/pricing.component';
+import { SignupComponent } from './components/signup/signup.component';
 
 export const routes: Routes = [
 
   { path: 'pricing', component: PricingComponent },
+  { path: 'signup', component: SignupComponent },
   { path: '**', redirectTo: '' } // Wildcard route
 ];
--- src/app/components/pricing/pricing.component.ts
+++ src/app/components/pricing/pricing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,FooterComponent],
   templateUrl: './pricing.component.html',
   styleUrls: ['./pricing.component.css']
 })
