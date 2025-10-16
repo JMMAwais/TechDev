a5a9d314b008310473e7f5b5f91725eaa01bba41
JMMAwais <jmm.awaiskiyani@gmail.com>
2025-10-16 21:40:52 +0500

    login component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes , RouterModule} from '@angular/router';
 import { PricingComponent } from './components/pricing/pricing.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component'
 import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
 
   { path: 'pricing', component: PricingComponent },
   { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
   { path: '**', redirectTo: '' } // Wildcard route
 ];
 
