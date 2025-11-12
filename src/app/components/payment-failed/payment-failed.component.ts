2565608cf28e22a4ae4da2ada682845bbf541d4d
JMMAwais <jmm.awaiskiyani@gmail.com>
2025-11-12 11:42:12 +0500

    updated payment succes ui 12

---
+++ src/app/components/payment-failed/payment-failed.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-payment-failed',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './payment-failed.component.html',
+  styleUrls: ['./payment-failed.component.css']
+})
+export class PaymentFailedComponent {}
+
